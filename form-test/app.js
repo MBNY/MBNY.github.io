3ca943d408c0349a5d411fbb39d43106694cb5b2
MBNY <baker.m@hotmail.com>
2016-04-13 21:25:46 -0400

    cash register HW

---
+++ form-test/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+$('#myForm').submit(function(event){
+	event.preventDefault();
+
+	var myInput = $('#name-input').val();
+
+	$('#target').text(myInput);
+
+	})
+
+})
