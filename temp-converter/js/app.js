a143e5cade86397a48ea26281b72a0d4b542fe1a
MBNY <baker.m@hotmail.com>
2016-04-13 20:18:08 -0400

    Class 13

---
+++ temp-converter/js/app.js
@@ -0,0 +1,18 @@
+$('submit').click(function(){
+	
+})
+
+function cToF(celsius)   
+{  
+  var cTemp = celsius;  
+  var cToFahr = cTemp * 1.8 + 32;  
+    
+}  
+  
+function fToC(fahrenheit)   
+{  
+  var fTemp = fahrenheit;  
+  var fToCel = (fTemp - 32) * 1.8;  
+    
+}   
+
