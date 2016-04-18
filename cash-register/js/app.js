93f7318536dca92591540467d0e484baaa74ee69
MBNY <baker.m@hotmail.com>
2016-04-18 18:48:56 -0400

    coursework update

---
+++ cash-register/js/app.js
@@ -0,0 +1,32 @@
+// 1: Add a .submit event to the form; use an anonymous function to handle the sumbit
+// Inside the anonymous function:
+// 2: Remeber to prevent the default action on the form!
+// 3: Create a variable to store the user input from #newEntry
+// 4: Remember to call parseFloat() on this variable! It needs to be a number.
+// 5: Append a new <tr></tr> to the <tbody id="entries"> containing two <td>'s, one of which contains the new variable: <tr><td></td><td>NEW VALUE</td></tr>
+// 6: Figure out a way to update the number in #total (Hint: look back at the calculator box example)
+// 7: Empty the input in #newEntry (clear it out)
+
+$(document).ready(function () {
+
+	var sum = 0;
+
+	$('#entry').submit(function (event) {
+		
+		event.preventDefault();
+
+		var newEntry = $('#newEntry').val();
+
+		newEntry = parseFloat(newEntry);
+
+
+	$('#entries').append('<tr><td></td><td>' + newEntry + '</td></tr>');
+
+		sum = sum + newEntry
+
+		$('#total').text('$' + sum);
+
+		$('#newEntry').val('')
+	});
+
+})
