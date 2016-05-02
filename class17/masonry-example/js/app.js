05564c8b3a9b2b3ba88186398c1e6727d94eb689
MBNY <baker.m@hotmail.com>
2016-05-02 19:42:28 -0400

    intro bootstrap

---
+++ class17/masonry-example/js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+	
+	$('.grid')masonry({})
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+
+$('.grid').on('click', '.grid-item', function() {
+
+	$('.grid').masonry('remove', this).masonry('layout');
+
+	});
+
+}
+
+
